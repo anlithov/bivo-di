7d725b6af82822e186783126baa548863ae55afa
anlithov <anlithov@gmail.com>
2025-01-30 12:23:23 +0200

    Missing returns

--- di/helpers/container.resolver.ts
+++ di/helpers/container.resolver.ts
@@ -9,12 +9,12 @@ const getContainerMeta = (container: Class) => {
   return container[Symbol.metadata] as ContainerDecoratorContext["metadata"];
 };
 
-export const registerContainer = async (container: Class) => {
+export const registerContainer = async (container: Class): Promise<void> => {
   const containerMeta = getContainerMeta(container);
   await containerMeta.__container_reg_fn();
 };
 
-export const shutdownContainer = async (container: Class) => {
+export const shutdownContainer = async (container: Class): Promise<void> => {
   const containerMeta = getContainerMeta(container);
   await containerMeta.__container_shutdown_fn();
 };
